c35092cce194de8c063d7bfee96b8ed59714ac9d
Pete Oottamakorn <poottamakorn@yahoo.com>
2024-05-04 21:39:30 -0600

    adding components, updating 'create vite' defaults

---
+++ React Portfolio/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function Footer() {
+    
+}
+
+export default Navigation;
---
+++ React Portfolio/src/components/Header.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function Header() {
+    const [] = useState('')
+}
+
+export default Header;
